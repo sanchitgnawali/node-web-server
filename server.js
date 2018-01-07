e7bff0a7f1462a946f5648d03839036453487c9a
sanchitgnawali <sanchitgnawali@gmail.com>
2018-01-07 17:39:42 -0600

    under construction middleware removed

--- server.js
+++ server.js
@@ -19,9 +19,9 @@ hbs.registerHelper('screamIt',(text)=>{
     return text.toUpperCase();
 });
 
-app.use((req,res,next)=>{
-    res.render('maintainance.hbs');
-});
+// app.use((req,res,next)=>{
+//     res.render('maintainance.hbs');
+// });
 
 app.use(express.static(__dirname+'/public'));
 
